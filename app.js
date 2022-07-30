eb92c5ea23e2c63e278c2f39d5f8003696508019
Gulov Azam <gulovazam@gmail.com>
2022-07-30 15:05:44 +0500

    Can dynamically change gridsize now.

--- app.js
+++ app.js
@@ -1,8 +1,22 @@
 const gridContainer = document.getElementById('gridContainer');
+let gridNumber = 16;
+let gridNumberSqr = gridNumber**2
 
 
-for (let i = 0; i !== 256; i++) {
+function createGrid() {
+for (let i = 0; i !== gridNumberSqr; i++) {
     const grid = document.createElement('div');
     grid.className = 'grid';
     gridContainer.appendChild(grid);
+    setGridSize(grid);
+  };
+};
+
+function setGridSize(gridElement) {
+    let containerSize = 512
+    let calcEm = (containerSize / gridNumberSqr) * gridNumber / 16;
+    gridElement.style.width = `${calcEm}em`
+    gridElement.style.height = `${calcEm}em`
 }
+
+createGrid()
