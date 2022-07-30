f024c32a048213eca8f17fa7a06d11837bfcd751
Gulov Azam <gulovazam@gmail.com>
2022-07-30 14:01:32 +0500

    Added 16x16 grid dynamically

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
 const gridContainer = document.getElementById('gridContainer');
+
+
+for (let i = 0; i !== 256; i++) {
+    const grid = document.createElement('div');
+    grid.className = 'grid';
+    gridContainer.appendChild(grid);
+}
