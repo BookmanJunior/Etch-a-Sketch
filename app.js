608e578fee697bf6b4d14f1b1debfc52dbecbf6a
Gulov Azam <gulovazam@gmail.com>
2022-07-31 19:34:46 +0500

    Added functionality to clear button

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
-const gridContainer = document.getElementById("gridContainer");
-const gridSizeBtn = document.getElementById("gridSize");
-const gridElements = gridContainer.getElementsByTagName("div");
+const gridContainer = document.getElementById('gridContainer');
+const gridElements = gridContainer.getElementsByTagName('div');
+const gridSizeBtn = document.getElementById('gridSizeBtn');
+const clearBtn = document.getElementById('clearBtn');
 
-gridSizeBtn.addEventListener("click", createNewGrid);
-gridContainer.addEventListener("mouseover", (e) => {
+
+gridSizeBtn.addEventListener('click', createNewGrid);
+clearBtn.addEventListener('click', clearGrid);
+gridContainer.addEventListener('mouseover', (e) => {
     if (e.target.className === 'grid') {
         e.target.className = 'grid painted';
     };
@@ -11,20 +14,20 @@ gridContainer.addEventListener("mouseover", (e) => {
 
 function defaultGrid() {
   for (let i = 0; i !== 16 ** 2; i++) {
-    const div = document.createElement("div");
-    div.className = "grid";
+    const div = document.createElement('div');
+    div.className = 'grid';
     gridContainer.appendChild(div);
   }
 }
 
 function createNewGrid() {
-  let newGridSize = Number(prompt("Enter a new size: ", "e.g 24"));
+  let newGridSize = Number(prompt('Enter a new size: ', 'e.g 24'));
 
   if (newGridSize >= 16 && newGridSize <= 100) {
     removeGridElements();
     for (let i = 0; i !== newGridSize ** 2; i++) {
-      const newDiv = document.createElement("div");
-      newDiv.className = "grid";
+      const newDiv = document.createElement('div');
+      newDiv.className = 'grid';
       gridContainer.appendChild(newDiv);
       setGridElementSize(newDiv, newGridSize);
     }
@@ -41,6 +44,12 @@ function setGridElementSize(gridElement, gridElementSize) {
 
 function removeGridElements() {
   Array.from(gridElements).forEach((gridElement) => gridElement.remove());
+}
+
+function clearGrid() {
+    Array.from(gridElements).forEach((gridElement) => {
+        gridElement.className = 'grid';
+    });
 }
 
 defaultGrid();
